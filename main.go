e93c27fc8030c1d75789af7682cdd3290b3b750d
aleparuokakauppa <aleksileinonen2@gmail.com>
2024-02-10 17:46:37 +0200

    Refactoring code to be a lot more readable

--- main.go
+++ main.go
@@ -10,23 +10,26 @@ import (
 	"time"
 )
 
-/*
-Product csv indexes:
-Handle // Index 0
-Title // Index 1
-Vendor // Index 3
-Product_Category // Index 4
-Tags // Index 6
-Published // Index 7
-Variant_Price // Index 19
-Status // Index 48
 
-Inventory csv indexes:
-Handle // Index 0
-Title // Index 1
-SKU // Index 8
-Sales channels begin at 11:
+/*
+These are hardcoded because the import/export expects these indexes
 */
+const (
+    // Hardcoded product CSV indexes related to its header
+    handleIndex = 0
+    titleIndex = 1
+    vendorIndex = 3
+    productCategoryIndex = 4
+    tagsIndex = 6
+    publishedIndex = 7
+    variantPriceIndex = 19
+    statusIndex = 48
+    // - Inventory indexes
+
+    skuIndex = 8
+    // The first sales channel is at index 11
+    salesChannelSindex = 11
+)
 
 type Product struct {
     Handle string // Index 0
@@ -42,7 +45,7 @@ type Product struct {
 
 // Returns the Product's properties as a string slice
 // formatted with Shopify product indexes
-func (s Product) formatShopifyProduct() []string {
+func (s Product) formatToShopifyProduct() []string {
     // Fill the slice with data at their appropriate indexes
     var result []string
     result = append(result, s.Handle) // Index 0
@@ -83,19 +86,17 @@ func main() {
     var inventoryExportFileName string
     var outputFileName string
 
-    if len(os.Args) > 1 {
-        if os.Args[1] == "-h" {
-            println("This program is intended for use with Shopify product and inventory export files.")
-            println("Usage:")
-            println("   ./main [productExport.csv] [inventoryExport.csv] [outputFile.csv]")
-            println()
-            println("In this version source code must be modified for intended functionality")
-            println("It checks products that have a certain tag attached to them and archives them if they don't have inventory left")
-            println("The resulting file should be imported in Shopify admin")
-            println("The field 'Overwrite products with matching handles' should be enabled for changes to apply")
-            return
-        }
+    if len(os.Args) > 1 && os.Args[1] == "-h"{
+        println("This program is intended for use with Shopify product and inventory export files.")
+        println("Usage:")
+        println("   ./main [productExport.csv] [inventoryExport.csv] [outputFile.csv]")
+        println()
+        println("The program checks products that have a certain tag attached to them and archives them if they don't have enough inventory left")
+        println("The resulting file should be imported in Shopify admin")
+        println("The field 'Overwrite products with matching handles' should be enabled in shopify for changes to apply")
+        return
     } 
+
     if len(os.Args) < 4 {
         fmt.Printf("Not enough arguments. Need 3, got %d.\n", len(os.Args)-1)
         println("Usage:")
@@ -103,11 +104,11 @@ func main() {
         println()
         println("Give -h as the first argument for help")
         return
-    } else {
-        productExportFileName = os.Args[1]
-        inventoryExportFileName = os.Args[2]
-        outputFileName = os.Args[3]
     }
+
+    productExportFileName = os.Args[1]
+    inventoryExportFileName = os.Args[2]
+    outputFileName = os.Args[3]
 
     // Open the required files
     productExportFile, err := os.Open(productExportFileName)
@@ -125,63 +126,85 @@ func main() {
     productsReader := csv.NewReader(productExportFile)
     inventoryReader := csv.NewReader(inventoryExportFile)
 
-    // Read the CSV headers
     productCSVheader, err := productsReader.Read()
     if err != nil {
-        fmt.Printf("Products File couldn't be read\n")
-        panic(err.Error())
+        panic(fmt.Errorf("Product CSV-header couldn't be read: %v", err))
     }
-    _, err = inventoryReader.Read()
+    inventoryCSVheader, err := inventoryReader.Read()
     if err != nil {
-        fmt.Printf("Inventory File couldn't be read\n")
-        panic(err.Error())
+        panic(fmt.Errorf("Inventory CSV-header couldn't be read: %v", err))
     }
+
+    salesChannel := getSalesChannel(inventoryCSVheader)
+
     // Get products from CSV readers
-    products, err := getProducts(productsReader, inventoryReader)
+    products, err := getProducts(productsReader, inventoryReader, salesChannel)
     if err != nil {
         panic(err.Error())
     }
 
     scanner := bufio.NewScanner(os.Stdin)
-
     fmt.Println("What tags should be archived?")
     scanner.Scan()
-    illegalTagsString := scanner.Text()
-    illegalTags := strings.Fields(illegalTagsString)
+    illegalTags := strings.Fields(scanner.Text())
 
-    fmt.Println("Give minumum stock to not be archived")
+    fmt.Println("Give minumum stock to not be archived (if less than this value, it gets archived)")
     scanner.Scan()
-    minStockString := scanner.Text()
-    minStock, err := strconv.Atoi(minStockString)
+    minStock, err := strconv.Atoi(scanner.Text())
     if err != nil {
         fmt.Printf("Given minimum stock isn't an integer.\n")
-        panic(err.Error())
+        panic(err)
     }
-    fmt.Printf("Minimum stock: %d\n", minStock)
 
     timeStart := time.Now()
     fmt.Printf("Starting task @ %v\n", timeStart)
 
     archivedProducts := archiveWithTags(products, illegalTags, minStock)
 
-    if err != nil {
-        panic(err.Error())
-    }
-
-    var productStringSlice [][]string
+    var rawProductSlice [][]string
     for _, product := range archivedProducts {
-        productStringSlice = append(productStringSlice, product.formatShopifyProduct())
+        rawProductSlice = append(rawProductSlice, product.formatToShopifyProduct())
     }
 
-    writeCSV(productStringSlice, productCSVheader, outputFileName)
+    writeCSV(rawProductSlice, productCSVheader, outputFileName)
 
     timeDelta := time.Now().UnixMilli() - timeStart.UnixMilli()
     fmt.Printf("Task finished @ %v, took %d milliseconds\n", time.Now(), timeDelta)
 }
 
+/*
+Returns the sales channel index in the inventoryCSVheader
+*/
+func getSalesChannel(inventoryCSVheader []string) (channel int) {
+    var channels []string
+    for i := salesChannelSindex; i < len(inventoryCSVheader)-1; i++ {
+        channels = append(channels, inventoryCSVheader[i])
+    }
+    scanner := bufio.NewScanner(os.Stdin)
+    fmt.Println("What sales channel should be used?")
+    for i, channel := range channels {
+        fmt.Printf("%d: %s\n", i, channel)
+    }
+    // Loop for getting a proper ID
+    for {
+        fmt.Println("Give channel ID:")
+        var err error
+        scanner.Scan()
+        channel, err = strconv.Atoi(scanner.Text())
+        if err == nil {
+            break
+        }
+        fmt.Println("Given channel isn't an integer. Try again")
+    }
+    return channel
+}
 
-// Writes the data as new lines into a CSV file with csvHeader as its headers.
-// filename is the new CSV-file's name
+
+/*
+Writes the data as new lines into a CSV file with csvHeader as its headers.
+
+Filename is the new CSV-file's name
+*/
 func writeCSV(data [][]string, csvHeader []string, filename string) error {
     newCSV, err := os.Create(filename)
     if err != nil {
@@ -195,6 +218,9 @@ func writeCSV(data [][]string, csvHeader []string, filename string) error {
     return nil
 }
 
+/*
+Returns products that have `tags` and which have less stock than minStock
+*/
 func archiveWithTags(products []Product, tags []string, minStock int) (arcivedProducts []Product) {
     for _, product := range products {
         for _, tag := range tags {
@@ -207,66 +233,64 @@ func archiveWithTags(products []Product, tags []string, minStock int) (arcivedPr
     return arcivedProducts
 }
 
-func getProducts(productsReader *csv.Reader, inventoryReader *csv.Reader) (products []Product, err error) {
+/*
+Returns all products read from pReader with its inventory values read from iReader
+*/
+func getProducts(pReader *csv.Reader, iReader *csv.Reader, salesChannel int) (products []Product, err error) {
     // Append the inventory into inventoryRecord
-    inventoryRecord, err := inventoryReader.ReadAll()
+    inventoryRecord, err := iReader.ReadAll()
     if err != nil {
-        return products, err
+        return products, fmt.Errorf("Inventory couldn't be read: %v", err)
     }
-    productRecord, err := productsReader.ReadAll()
+    productRecord, err := pReader.ReadAll()
     if err != nil {
-        return products, err
+        return products, fmt.Errorf("Products couldn't be read: %v", err)
     }
 
-    for _, product := range productRecord {
-        formattedProduct, err := getProductFormatted(product, inventoryRecord)
+    for _, rawProduct := range productRecord {
+        product := formatToProduct(rawProduct)
+        product.Inventory, err = getInventory(product.Handle, salesChannel, inventoryRecord)
         if err != nil {
-            return products, err
+            if err == fmt.Errorf("404") {
+                return products, fmt.Errorf("Product %s handle not found in the inventoryRecord", product.Handle)
+            } else {
+                return products, err
+            }
         }
-        products = append(products, formattedProduct)
+        products = append(products, product)
     }
 
     return products, nil
 }
 
-func getProductFormatted(unformattedProduct []string, inventoryRecord [][]string) (product Product, err error) {
-    // Append the values into the new product
-    product.Handle = unformattedProduct[0]
-    product.Title = unformattedProduct[1]
-    product.Status = unformattedProduct[48]
-    product.Tags = strings.Split(unformattedProduct[6], ", ")
-
-    // Check the product's inventory
-    productInventoryString := getInventory(product.Handle, inventoryRecord)
-    if err != nil {
-        if productInventoryString != "not stocked" {
-            return product, err
-        } else {
-            product.Inventory = 0
-        }
-    } else {
-        product.Inventory, err = strconv.Atoi(productInventoryString)
-        if err != nil {
-            return product, err
-        }
-        if product.Inventory == -404 {
-            // Handle not found in the inventory file
-        }
-    }
-    return product, nil
+/*
+Assigns the values from hardcoded indexes into the product
+*/
+func formatToProduct(rawProduct []string) (product Product) {
+    product.Handle = rawProduct[handleIndex]
+    product.Title = rawProduct[titleIndex]
+    product.Status = rawProduct[statusIndex]
+    product.Tags = strings.Split(rawProduct[tagsIndex], ", ")
+    return product
 }
 
-// Gets the value (inventory) at index 12 if handle is found
-// If not found, returned inventory is -404 not found
-func getInventory(handle string, inventoryRecord [][]string) (inventory string) {
+/*
+Gets the value (inventory) at salesChannel index if handle is found
+*/
+func getInventory(handle string, salesChannel int, inventoryRecord [][]string) (inventory int, err error) {
+    // Correct for the index offset
+    salesChannel += salesChannelSindex
     for _, record := range inventoryRecord {
-        if handle == record[0] {
-            if record[12] == "not stocked" {
-                return "0"
-            } else {
-                return record[12]
+        if handle == record[handleIndex] {
+            if record[salesChannel] == "not stocked" {
+                return 0, nil
             }
+            inventory, err = strconv.Atoi(record[salesChannel])
+            if err != nil {
+                return 0, fmt.Errorf("Cannot convert inventory stock value found with handle: %s", handle);
+            }
+            return inventory, nil
         }
     }
-    return "-404"
+    return -1, nil
 }
